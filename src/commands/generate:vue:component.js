5e6faf371ec62be7d3912fe7895d47cde6efcc4b
lukyhenson <lukyhenson@gmail.com>
2019-05-09 01:24:41 -0300

    It creates vue component generator

---
+++ src/commands/generate:vue:component.js
@@ -0,0 +1,28 @@
+module.exports = {
+  name: 'generate:vue:component',
+  alias: ['gvc'],
+  description: 'This command will create a vue component, [--path] [--type] [--styled]',
+  run: async toolbox => {
+    const { 
+      parameters: { first, },
+      print: { error, success },
+      template: { generate },
+      strings: { kebabCase, pascalCase }, 
+    } = toolbox;
+
+    if (!first) {
+      error('Component name must be supplied :(');
+    } else {      
+      const componentName = pascalCase(first);
+      const cssComponentName = kebabCase(first);
+
+      await generate({
+        template: 'vue-component.js.ejs',
+        target: `src/components/${componentName}/index.vue`,
+        props: { name: componentName, cssComponentName }
+      });
+      
+      success(`Component ${componentName} was generated!`);
+    }
+  },
+};
